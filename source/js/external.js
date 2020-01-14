f84097e5e468f5bd8d338ff31f27895f29da8e7c
VladislavNovak <novaldix@gmail.com>
2020-01-14 09:46:16 +0300

    меняет бургер открытия/закрытия меню

--- source/js/external.js
+++ source/js/external.js
@@ -3,17 +3,17 @@ function executeMain() {
     var logotypeButton = document.querySelector(".logotype__button");
     var navigation = document.querySelector(".navigation");
 
-    logotypeButton.addEventListener("click", function() {
-      if(navigation.classList.contains("navigation--opened") && logotypeButton.classList.contains("logotype__button--burger")) {
+    logotypeButton.addEventListener("click", function(e) {
+      e.preventDefault();
+      // (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+      if (navigation.classList.contains("navigation--opened")) {
         navigation.classList.remove("navigation--opened");
         navigation.classList.add("navigation--closed");
-        logotypeButton.classList.remove("logotype__button--burger");
-        logotypeButton.classList.add("logotype__button--cross");
+        this.classList.add("is-active");
       } else {
         navigation.classList.add("navigation--opened");
         navigation.classList.remove("navigation--closed");
-        logotypeButton.classList.add("logotype__button--burger");
-        logotypeButton.classList.remove("logotype__button--cross");
+        this.classList.remove("is-active");
       }
     });
   }
